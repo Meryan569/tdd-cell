373be9a8daf3ff586c41bf362cf5cadc0c66f018
Melissa Ryan <meryan569@gmail.com>
2016-02-29 18:54:49 -0800

    disallow name changes in cell

--- cell.rb
+++ cell.rb
@@ -1,2 +1,9 @@
 class Cell
+	attr_reader :name
+	def initialize (name = "Bob")
+		@name = name
+	end
+	
+	def name=(arg)
+	end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,7 +4,14 @@ describe Cell do
   describe '#name' do
     it 'returns the name given in the initializer' do
       cell = Cell.new('Bob')
-      cell.name.should eq 'Bob'
+      expect(cell.name).to eq 'Bob'
     end
+
+    it 'does not allow name changes' do
+    	cell = Cell.new('Bob')
+    	cell.name = 'Mark'
+    	expect(cell.name).to eq 'Bob'
+    end
+
   end
 end
